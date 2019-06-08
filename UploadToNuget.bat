bea84ec0a520d77ed627ef675f5133d479651245
Harold E Collins <masterskriptor@hotmail.com>
2019-06-07 19:54:47 -0500

    stoping the doxing of myself...

--- UploadToNuget.bat
+++
@@ -1,4 +0,0 @@
-dotnet nuget push src\Core\bin\debug\EXCSLA.Shared.Core.1.0.4.nupkg -k oy2pnqr2iqvilymjmnqaxsxwm2f7ia7u7bd66dfdtjlfvi -s https://api.nuget.org/v3/index.json
-dotnet nuget push src\EXCSLA.Shared.Core.Specifications\bin\debug\EXCSLA.Shared.Core.Specifications.1.0.4.nupkg -k oy2pnqr2iqvilymjmnqaxsxwm2f7ia7u7bd66dfdtjlfvi -s https://api.nuget.org/v3/index.json
-dotnet nuget push src\ValueObjects.Common\bin\debug\EXCSLA.Shared.Core.ValueObjects.Common.1.0.4.nupkg -k oy2pnqr2iqvilymjmnqaxsxwm2f7ia7u7bd66dfdtjlfvi -s https://api.nuget.org/v3/index.json
-dotnet nuget push src\EXCSLA.Shared.Infrastructure.Data\bin\debug\EXCSLA.Shared.Infrastructure.Data.1.0.4.nupkg -k oy2pnqr2iqvilymjmnqaxsxwm2f7ia7u7bd66dfdtjlfvi -s https://api.nuget.org/v3/index.json

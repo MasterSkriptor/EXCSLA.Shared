a1033e6e3615bbdcd765f457fb0c73a042d2d945
Harold E Collins <masterskriptor@hotmail.com>
2019-12-08 11:31:28 -0600

    Added assembly for Domain Event Dispatcher.

---
+++ src/Infrastructure/DomainEventDispatcher/DomainEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using EXCSLA.Shared.Core;
+using System.Threading.Tasks;
+
+namespace EXCSLA.Shared.Infrastructure.Services
+{
+    public partial class DomainEventDispatcher
+    {
+        private abstract class DomainEventHandler
+        {
+            public abstract void Handle(BaseDomainEvent domainEvent);
+            public abstract Task HandleAsync(BaseDomainEvent domainEvent);
+        }
+    }
+}

71d88232f6330eebc4d31719deb2ed5e263a9005
Harold E Collins <masterskriptor@hotmail.com>
2019-12-08 11:31:57 -0600

    Added Assembly for SendGrid as an email sender service.

---
+++ src/Infrastructure/SendGridEmailService/SendGridEmailClient.cs
@@ -0,0 +1,36 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using EXCSLA.Shared.Core.Interfaces;
+using System.Threading.Tasks;
+
+namespace EXCSLA.Shared.Infrastructure
+{
+    public class SendGridEmailClient : IEmailSender
+    {
+        private readonly string _tempApiKey;
+
+        public SendGridEmailClient(SendGridOptions options)
+        {
+            _tempApiKey = options.Key;
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string message)
+        {
+            await SendEmailAsync(email, subject, message, "he@excsla.com");
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string message, string from)
+        {
+            var client = new SendGridClient(_tempApiKey);
+            var msg = new SendGridMessage();
+
+            msg.SetFrom("donotreply@mamatara.org");
+            msg.AddTo(email);
+            msg.SetReplyTo(new EmailAddress(from));
+            msg.SetSubject(subject);
+            msg.AddContent(MimeType.Html, message);
+
+            await client.SendEmailAsync(msg);
+        }
+    }
+}
---
+++ src/Infrastructure/SendGridEmailService/SendGridOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace EXCSLA.Shared.Infrastructure
+{
+    public class SendGridOptions
+    {
+        private string _sendGridApiKey;
+
+        public string Key { get { return _sendGridApiKey; } set { _sendGridApiKey = value; } }
+
+        public SendGridOptions() { }
+    }
+}

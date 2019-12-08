75b9b8e16d47dd2039c1c429c5ca675884a4ccfc
Harold E Collins <masterskriptor@hotmail.com>
2019-12-08 11:25:14 -0600

    moved base ddd objects to its own assembly.

---
+++ src/Core/DomainModels/AggregateRoot.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+
+namespace EXCSLA.Shared.Core
+{
+    public abstract class AggregateRoot : BaseEntity
+    {
+        private readonly List<BaseDomainEvent> _events = new List<BaseDomainEvent>();
+        public virtual IReadOnlyList<BaseDomainEvent> Events => _events;
+
+        protected virtual void AddDomainEvent(BaseDomainEvent newEvent)
+        {
+            _events.Add(newEvent);
+        }
+
+        public virtual void ClearEvents()
+        {
+            _events.Clear();
+        }
+    }
+}

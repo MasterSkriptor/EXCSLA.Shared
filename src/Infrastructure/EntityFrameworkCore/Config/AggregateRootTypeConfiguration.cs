2e0f3a103c78320a8825c5e87697ee5c553de120
Harold E Collins <masterskriptor@hotmail.com>
2019-12-08 11:28:18 -0600

    Reworked the EntityFramework data assemblies. Added one for Identity, and one for ef without identity (standard).

---
+++ src/Infrastructure/EntityFrameworkCore/Config/AggregateRootTypeConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using EXCSLA.Shared.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EXCSLA.Shared.Infrastructure.Data.EntityFrameworkCore.Config
+{
+    public abstract class AggregateRootTypeConfiguration<T> : IEntityTypeConfiguration<T> where T : AggregateRoot
+    {
+        public virtual void Configure(EntityTypeBuilder<T> builder)
+        {
+            // Attach sql properties
+            CommonSqlProperties.Build(builder);
+
+            // Ignore Aggregate Events
+            builder.Ignore(c => c.Events);
+        }
+    }
+}
---
+++ src/Infrastructure/EntityFrameworkCore/Config/CommonSqlProperties.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace EXCSLA.Shared.Infrastructure.Data.EntityFrameworkCore.Config
+{
+    public static class CommonSqlProperties
+    {
+        public static void Build(EntityTypeBuilder builder)
+        {
+            builder.Property<DateTime>("CreatedDate")
+                .HasDefaultValueSql("GetDate()");
+            builder.Property<byte[]>("Version")
+                .IsRowVersion();
+        }
+    }
+}

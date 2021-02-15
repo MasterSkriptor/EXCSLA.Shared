1a15dda10902b655da679dc6c8cd7109d0436fd3
Harold Collins <masterskriptor@hotmail.com>
2021-02-15 14:07:09 -0600

    Updated all Infrastructure projects to dotnet 5.0, and uploaded to nuget.

--- src/Infrastructure/AlertService/AlertService.cs
+++ src/Infrastructure/AlertService/AlertService.cs
@@ -1,9 +1,7 @@
 ﻿using EXCSLA.Shared.Core.Interfaces;
 using EXCSLA.Shared.Core.ValueObjects.Common;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace EXCSLA.Shared.Infrastructure.Services
 {

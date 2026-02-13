e18a2503cac4fffda67cdbe3c23d1fc4242203e0
Harold Collins <masterskriptor@hotmail.com>
2026-02-12 21:35:48 -0600

    feat: Create EXCSLA.Shared.Core project and migrate base domain classes

--- src/Core/EXCSLA.Shared.Core/BaseDomainEvent.cs
+++ src/Core/EXCSLA.Shared.Core/BaseDomainEvent.cs
@@ -2,11 +2,10 @@ using System;
 
 namespace EXCSLA.Shared.Core;
 
-
 /// <summary>
 /// Base class for domain event. Adds DateOccured to the event for tracking when the event occured.
 /// </summary>
 public abstract class BaseDomainEvent
 {
-    public DateTime DateOccured {get; protected set; } = DateTime.UtcNow;
+	public DateTime DateOccured {get; protected set; } = DateTime.UtcNow;
 }
